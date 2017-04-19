a7aae7c692239e58ab32d4935de14e6440ebb91d
straightway <straightway@users.noreply.github.com>
2017-04-18 23:18:24 -0620

    Disabled compiler warning after review

--- general/src/straightway/general/dsl/Utilities.kt
+++ general/src/straightway/general/dsl/Utilities.kt
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  ****************************************************************************/
+@file:Suppress("UNCHECKED_CAST")
+
 package straightway.general.dsl
 
 fun <TArg, TResult> untyped(typed: (TArg) -> TResult): (Any) -> Any
